07fc15c89cc79a823161ebe7c480576780fff68e
Mario Karagiorgas <salem8@gmail.com>
2020-11-17 19:26:40 +0200

    Streamline persons collection

--- gcpfunc/store.go
+++ gcpfunc/store.go
@@ -12,8 +12,9 @@ import (
 )
 
 const (
-	root      = "church"
-	personCol = "person"
+	users    = "users"
+	meetings = "meetings"
+	bookings = "bookings"
 )
 
 func newFirebaseCfg() *firebase.Config {
@@ -29,8 +30,8 @@ type person struct {
 	First       string    `firestore:"first,omitempty"`
 	Last        string    `firestore:"last,omitempty"`
 	Subscribed  bool      `firestore:"subscribed"`
-	PhoneNumber string    `firestore:"phoneNumber,omitempty"`
-	Type        string    `firestore:"type,omitempty"`
+	PhoneNumber string    `firestore:"phoneNumber"`
+	Type        string    `firestore:"type"`
 	Updated     time.Time `firestore:"updated,omitempty"`
 }
 
@@ -65,8 +66,8 @@ func (s *CloudStore) addUserIfNotExists(p *person) {
 	if p == nil {
 		return
 	}
-	church := s.db.Collection(root)
-	q := church.Where("id", "==", p.ID)
+	personsCol := s.db.Collection(users)
+	q := personsCol.Where("id", "==", p.ID)
 	iter := q.Documents(context.Background())
 	defer iter.Stop()
 	doc, err := iter.Next()
@@ -79,12 +80,14 @@ func (s *CloudStore) addUserIfNotExists(p *person) {
 			return
 		}
 	}
-	s.upsertPerson(church, p)
+	s.upsertPerson(personsCol, p)
 }
 
-func (s *CloudStore) upsertPerson(church *firestore.CollectionRef, p *person) {
-	// upsert
-	_, err := church.Doc(personCol).Set(context.Background(), p)
+func (s *CloudStore) upsertPerson(personsCol *firestore.CollectionRef, p *person) {
+	if p.ID == "" {
+		return
+	}
+	_, _, err := personsCol.Add(context.Background(), p)
 	if err != nil {
 		log.Printf("Firestore addUserIfNotExists error: %s\n", err)
 	}
--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.15
 require (
 	cloud.google.com/go/firestore v1.3.0
 	cloud.google.com/go/storage v1.12.0 // indirect
-	firebase.google.com/go v3.13.0+incompatible // indirect
 	firebase.google.com/go/v4 v4.1.0
 	github.com/joho/godotenv v1.3.0
+	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
+	golang.org/x/tools v0.0.0-20201013201025-64a9e34f3752 // indirect
 	google.golang.org/api v0.35.0
 	google.golang.org/appengine v1.6.7 // indirect
 )
